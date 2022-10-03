418c0851b4c3fa3c62cdd7a584475b3c472428aa
Diego Benitez <dabenitezd@unal.edu.co>
2022-10-03 09:19:12 -0500

    navbar actualizacion

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -5,13 +5,19 @@ $(document).ready(function () {
         event.preventDefault();
         autenticarUsuario();
     });
+    
+    $("#form-register").submit(function (event) {
+
+        event.preventDefault();
+        registrarUsuario();
+    });
 
 });
 
 function autenticarUsuario() {
 
-    let username = $("#usuario").val();
-    let contrasena = $("#contrasena").val();
+    let username = $("#email").val();
+    let contrasena = $("#password").val();
 
     $.ajax({
         type: "GET",
@@ -26,10 +32,53 @@ function autenticarUsuario() {
             if (parsedResult != false) {
                 $("#login-error").addClass("d-none");
                 let username = parsedResult['username'];
-                document.location.href = "home.html?username=" + username;
+                document.location.href = "home_prestador.html?username=" + username;
             } else {
                 $("#login-error").removeClass("d-none");
             }
         }
     });
+}
+
+function registrarUsuario() {
+
+    let username = $("#input-username").val();
+    let contrasena = $("#input-contrasena").val();
+    let contrasenaConfirmacion = $("#input-contrasena-repeat").val();
+    let nombre = $("#input-nombre").val();
+    let apellidos = $("#input-apellidos").val();
+    let email = $("#input-email").val();
+    let telefono = $("#input-telefono").val();
+
+    if (contrasena == contrasenaConfirmacion) {
+
+        $.ajax({
+            type: "GET",
+            dataType: "html",
+            url: "./ServletUsuarioRegister",
+            data: $.param({
+                username: username,
+                contrasena: contrasena,
+                nombre: nombre,
+                apellidos: apellidos,
+                email: email,
+                telefono: telefono
+            }),
+            success: function (result) {
+                let parsedResult = JSON.parse(result);
+
+                if (parsedResult != false) {
+                    $("#register-error").addClass("d-none");
+                    let username = parsedResult['username'];
+                    document.location.href = "home.html?username=" + username;
+                } else {
+                    $("#register-error").removeClass("d-none");
+                    $("#register-error").html("Error en el registro del usuario");
+                }
+            }
+        });
+    } else {
+        $("#register-error").removeClass("d-none");
+        $("#register-error").html("Las contraseñas no coinciden");
+    }
 }
